e292d2309e13ada2f6c1432fcfafb9ea8df16ea7
froas <froas.crp@gmail.com>
2022-06-28 15:21:21 +0900

    add delete user for admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,17 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update, :index, :destroy]
+  before_action :admin_user, only: [:destory]
+
+  def admin_user
+    redirect_to(root_path) unless current_user.admin?
+  end 
+
+  def logged_in_user 
+    unless !current_user.nil?
+      flash[:danger] = "Please Log in"
+      redirect_to new_user_session_url
+    end
+  end
   def show
     @user = User.find(params[:id]) 
   end 
