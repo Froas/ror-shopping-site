bb3c9b947f2bcb744e7e26c7cff90240e332b8b3
froas <froas.crp@gmail.com>
2022-07-01 14:23:29 +0900

    some fix

--- app/controllers/staffs/registrations_controller.rb
+++ app/controllers/staffs/registrations_controller.rb
@@ -1,7 +1,29 @@
-# frozen_string_literal: true
+
 
 class Staffs::RegistrationsController < Devise::RegistrationsController
   include Accessible
   skip_before_action :check_resource, except: [:new, :create]
- 
+
+  def show
+    @staff = Staff.find(params[:id])
+  end
+
+  def new
+    @staff = Staff.new
+    
+  end
+
+  def create
+    @staff = Staff.new(staff_params)
+      if @staff.save
+        flash[:success] = "Blog created"
+        redirect_to root_url
+      else
+        render 'new'
+      end
+    end
+
+    def staff_params
+      params.require(:staff).permit(:name, :email, :password, :password_confirmation)
+    end
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,5 +3,26 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   include Accessible
   skip_before_action :check_resource, except: [:destory]
- 
+ def create
+  @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Blog created"
+      redirect_to root_url
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
 end
